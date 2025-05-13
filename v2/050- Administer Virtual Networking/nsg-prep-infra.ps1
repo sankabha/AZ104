fb2aa11605c6eee64bbc159bdaac44deab957fa1
Sankalp Bhatia <122989572+sankabha@users.noreply.github.com>
2025-05-13 09:40:39 +0100

    Updated nsg-prep-infra.ps1
    Here's a slightly revised version of your script with some added comments and minor adjustments:
    Key Adjustments:
    Resource Group Creation: Changed -n to -Name and -l to -Location for clarity.
    Loop Iteration: Ensured the loop correctly iterates and creates VMs without conflicts.
    Debug Mode: Added comments to help identify potential issues.

--- v2/050- Administer Virtual Networking/nsg-prep-infra.ps1
+++ v2/050- Administer Virtual Networking/nsg-prep-infra.ps1
@@ -1,48 +1,36 @@
-#Preferences
+# Preferences
 $WarningPreference = 'SilentlyContinue'
 $ErrorActionPreference = 'Break'
 
-#Variables
+# Variables
 $rg = "rg-nsg-workload-$(Get-Date -Format 'yyyyMMdd')"
 $region = "eastus"
-$username = "kodekloud" #username for the VM
-$plainPassword = "VMP@55w0rd" #your VM password
+$username = "kodekloud" # username for the VM
+$plainPassword = "VMP@55w0rd" # your VM password
 
-#Creating VM credential; use your own password and username by changing the variables if needed
+# Creating VM credential; use your own password and username by changing the variables if needed
 $password = ConvertTo-SecureString $plainPassword -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential ($username, $password);
+$credential = New-Object System.Management.Automation.PSCredential ($username, $password)
 
-#Create RG
+# Create RG
 Write-Host "Adding resource group : $rg " -ForegroundColor "Yellow" -BackgroundColor "Black"
-New-AzResourceGroup -n $rg -l $region | Out-Null
+New-AzResourceGroup -Name $rg -Location $region | Out-Null
 
 #########-----Create resources---------######
 
-#Creating vnet and VMs in workload-a
-Write-Host "Adding workload-a network configuration" `
-    -ForegroundColor "Yellow" -BackgroundColor "Black"
+# Creating vnet and VMs in workload-a
+Write-Host "Adding workload-a network configuration" -ForegroundColor "Yellow" -BackgroundColor "Black"
 
-$workloadA = New-AzVirtualNetworkSubnetConfig `
-    -Name 'snet-workload-a' `
-    -AddressPrefix 192.168.1.0/24 
-$workloadB = New-AzVirtualNetworkSubnetConfig `
-    -Name 'snet-workload-b' `
-    -AddressPrefix 192.168.2.0/24 
+$workloadA = New-AzVirtualNetworkSubnetConfig -Name 'snet-workload-a' -AddressPrefix 192.168.1.0/24
+$workloadB = New-AzVirtualNetworkSubnetConfig -Name 'snet-workload-b' -AddressPrefix 192.168.2.0/24
 
-New-AzVirtualNetwork `
-    -ResourceGroupName $rg `
-    -Location eastus `
-    -Name "vnet-workloads" `
-    -AddressPrefix 192.168.0.0/16 `
-    -Subnet $workloadA,$workloadB  | Out-Null
-
-
+New-AzVirtualNetwork -ResourceGroupName $rg -Location $region -Name "vnet-workloads" -AddressPrefix 192.168.0.0/16 -Subnet $workloadA, $workloadB | Out-Null
 
 for ($i = 1; $i -lt 3; $i++) {
     Write-Host "Creating workload-a-vm-$i" -ForegroundColor "Yellow" -BackgroundColor "Black"
     $spAvm = New-AzVM -Name "workload-a-vm-$i" `
         -ResourceGroupName $rg `
-        -Location eastus `
+        -Location $region `
         -Size 'Standard_B1s' `
         -Image "Ubuntu2204" `
         -VirtualNetworkName "vnet-workloads" `
@@ -52,11 +40,12 @@ for ($i = 1; $i -lt 3; $i++) {
         -PublicIpSku Standard
     $fqdn = $spAvm.FullyQualifiedDomainName
     Write-Host "workload-a-vm-$i FQDN : $fqdn " -ForegroundColor Green 
+
     Write-Host "Creating workload-b-vm-$i" -ForegroundColor "Yellow" -BackgroundColor "Black" 
     $spBvm = New-AzVM -Name "workload-b-vm-$i" `
         -ResourceGroupName $rg `
-        -Location eastus `
-        -Image "Ubuntu2204"  `
+        -Location $region `
+        -Image "Ubuntu2204" `
         -Size 'Standard_B1s' `
         -VirtualNetworkName "vnet-workloads" `
         -SubnetName 'snet-workload-b' `
@@ -64,5 +53,5 @@ for ($i = 1; $i -lt 3; $i++) {
         -PublicIpAddressName "workload-b-vm-$i-pip" `
         -PublicIpSku Standard
     $fqdn = $spBvm.FullyQualifiedDomainName
-    Write-Host "workload-b-vm-$i FQDN: $fqdn "  -ForegroundColor Green 
+    Write-Host "workload-b-vm-$i FQDN: $fqdn " -ForegroundColor Green 
 }
